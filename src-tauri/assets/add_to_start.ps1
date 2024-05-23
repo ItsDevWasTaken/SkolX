5c57df7794add8548b61feb5649f4bc6bbb9760f
ItsDevWasTaken <edvinduartemartins11@gmail.com>
2024-05-23 16:12:25 +0200

    Added autostart to Rainmeter

--- src-tauri/assets/add_to_start.ps1
+++ src-tauri/assets/add_to_start.ps1
@@ -1,4 +1,7 @@
 $shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut("$env:appdata\\Microsoft\\Windows\\Start Menu\\Programs\\Rainmeter.lnk")
-$shortcut.TargetPath = "C:\\Rainmeter\\Rainmeter.exe"
-$shortcut.Save()
+$start_shortcut = $shell.CreateShortcut("$env:appdata\\Microsoft\\Windows\\Start Menu\\Programs\\Rainmeter.lnk")
+$start_shortcut.TargetPath = "C:\\Rainmeter\\Rainmeter.exe"
+$start_shortcut.Save()
+$auto_shortcut = $shell.CreateShortcut("$env:appdata\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Rainmeter.lnk")
+$auto_shortcut.TargetPath = "C:\\Rainmeter\\Rainmeter.exe"
+$auto_shortcut.Save()
